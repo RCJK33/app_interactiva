feb5e334b8d3c24bbe09bd3f1ff7fc7cc85eabe9
Rafael Chavez Juarez <66058913+RCJK33@users.noreply.github.com>
2024-04-15 13:26:16 -0700

    Update js logic files

---
+++ js/main2.js
@@ -0,0 +1,43 @@
+
+const stack = ['0_0', 'X_X', '*_*'];
+const queue = ['0_0', 'X_X', '*_*'];
+const stackEl = document.getElementById("stack");
+const queueEl = document.getElementById("queue");
+
+function renderStack() {
+  stackEl.innerHTML = "";
+  for (let i = stack.length - 1; i >= 0; i--) {
+    const li = document.createElement("li");
+    li.textContent = stack[i];
+    stackEl.appendChild(li);
+  }
+}
+
+function renderQueue() {
+  queueEl.innerHTML = "";
+  for (let i = 0; i < queue.length; i++) {
+    const li = document.createElement("li");
+    li.textContent = queue[i];
+    queueEl.appendChild(li);
+  }
+}
+
+
+document.getElementById("queue-enqueue").addEventListener("click", () => {
+  const value = prompt("Enter a value to enqueue:");
+  queue.push(value);
+  renderQueue();
+});
+
+document.getElementById("queue-dequeue").addEventListener("click", () => {
+  if (queue.length === 0) {
+    alert("Queue is empty!");
+    return;
+  }
+  queue.shift();
+  renderQueue();
+});
+
+renderStack();
+
+//renderQueue();
